95d5e4f36fe9c1d61efee42244b66d876a94872d
Matthew Bucy <mbucy347@gmail.com>
2024-03-23 17:09:45 -0400

    bout to break stuff

--- frontend/src/pages/App.jsx
+++ frontend/src/pages/App.jsx
@@ -1,6 +1,9 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import Overview from './Overview.jsx';
 import Calculator from './Calculator.jsx';
+import Flow from './custom_nodes.jsx';
+import AllControls from './controls.jsx';
+import Formula from './CreateFormula.jsx';
 
 export default function App() {
   return (
@@ -9,6 +12,9 @@ export default function App() {
         <Route path='/' element={<Navigate to='/overview' />} />
         <Route path='/overview' element={<Overview />} />
         <Route path='/calculator' element={<Calculator />} />
+        <Route path='/controls' element={<AllControls />} />
+        <Route path='/flow' element={<Flow />} />
+        <Route path='/formula' element={<Formula />} />
       </Routes>
     </div>
   );
---
+++ frontend/src/pages/CalculateNode.jsx
@@ -0,0 +1,30 @@
+import { useCallback } from 'react';
+import { Handle, Position } from 'reactflow';
+
+const handleStyle = { left: 10 };
+
+export default function FormulaNode({ data, isConnectable }) {
+  const onChange = useCallback((evt) => {
+    // console.log(evt.target.value);
+    data.value = evt.target.value;
+    console.log(data.value);
+  }, []);
+
+  return (
+    <div className="text-updater-node">
+      {/* <Handle type="target" position={Position.Top} isConnectable={isConnectable} /> */}
+      <div>
+        <label htmlFor="text">Calculate</label>
+        <input id="text" name="text" onChange={onChange} className="nodrag" />
+      </div>
+      <Handle
+        type="target"
+        position={Position.Left}
+        id="a"
+        // style={handleStyle}
+        isConnectable={isConnectable}
+      />
+      <Handle type="source" position={Position.Right} id="b" isConnectable={isConnectable} />
+    </div>
+  );
+}
