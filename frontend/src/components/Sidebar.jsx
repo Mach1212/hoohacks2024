37f6e577c93b0cd53d4ec52a5259bbbe9231507a
Mach1212 <maciej.pruchnik@gmail.com>
2024-03-23 19:23:27 -0400

    [Feat] Add test data for dragging

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,13 +1,22 @@
-import { IconButton, Select } from '@mui/material';
+import { IconButton, MenuItem, Select, Typography } from '@mui/material';
 import React from 'react';
 import SaveIcon from '@mui/icons-material/Save';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import AddIcon from '@mui/icons-material/Add';
 import { Option } from '@mui/base/Option';
 
+const functions = [
+  { name: 'sin', type: 'number', value: '1' },
+  {
+    name: 'cos',
+    type: 'function',
+    func: () => console.log('Called'),
+  },
+];
+
 export default function Sidebar() {
   return (
-    <section className='p-10 max-w-[250px] bg-[#757575] rounded'>
+    <section className={'p-10 w-full max-w-[250px] bg-[#757575] rounded'}>
       <div className='flex flex-row justify-between'>
         <div>
           <IconButton className='bg-white rounded'>
@@ -21,13 +30,21 @@ export default function Sidebar() {
           <PlayArrowIcon />
         </IconButton>
       </div>
-      <Select>
-        <Select defaultValue={10}>
-          <Option value={10}>Ten</Option>
-          <Option value={20}>Twenty</Option>
-          <Option value={30}>Thirty</Option>
-        </Select>
+      <Select className='w-full mt-10 bg-white' value={10}>
+        <MenuItem value={10}>Values</MenuItem>
+        <MenuItem value={20}>Trig</MenuItem>
+        <MenuItem value={30}>Statistics</MenuItem>
       </Select>
+      <div className='flex flex-col w-full mt-4 gap-4'>
+        {functions.map((data) => (
+          <Typography
+            key={data}
+            variant='body1'
+            className='bg-white rounded w-full text-center py-4'>
+            {data.name}
+          </Typography>
+        ))}
+      </div>
     </section>
   );
 }
