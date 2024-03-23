1e77c15cbc13f9ef1504ff981eca8f9824ed15b3
Mach1212 <maciej.pruchnik@gmail.com>
2024-03-23 13:07:25 -0400

    [Feat] Add calculator to path

--- frontend/src/pages/App.jsx
+++ frontend/src/pages/App.jsx
@@ -1,5 +1,6 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import Overview from './Overview.jsx';
+import Calculator from './Calculator.jsx';
 
 export default function App() {
   return (
@@ -7,6 +8,7 @@ export default function App() {
       <Routes>
         <Route path='/' element={<Navigate to='/overview' />} />
         <Route path='/overview' element={<Overview />} />
+        <Route path='/calculator' element={<Calculator />} />
       </Routes>
     </div>
   );
