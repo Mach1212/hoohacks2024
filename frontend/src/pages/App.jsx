24ed4ab5f843b724cf95e2cb6c6371ea2e4be8ca
Mach1212 <maciej.pruchnik@gmail.com>
2024-03-23 15:02:52 -0400

    [Feat] Fix app.jsx

--- frontend/src/pages/App.jsx
+++ frontend/src/pages/App.jsx
@@ -4,7 +4,7 @@ import Calculator from './Calculator.jsx';
 
 export default function App() {
   return (
-    <div className='px-2'>
+    <div className='bg-[background] w-screen h-screen'>
       <Routes>
         <Route path='/' element={<Navigate to='/overview' />} />
         <Route path='/overview' element={<Overview />} />
