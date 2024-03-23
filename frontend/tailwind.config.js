a54c80593ac9b1e531dfe02ed158897760182fdc
Mach1212 <maciej.pruchnik@gmail.com>
2024-03-23 14:38:50 -0400

    [Feat] Make color scheme

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  important: "#root",
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  important: '#root',
   theme: {
-    extend: {},
+    colors: {
+      primary: '#757575',
+      accent: '#ffcd29',
+      text: '#233128',
+      background: '#e6e6e6',
+    },
   },
   plugins: [],
   corePlugins: {
